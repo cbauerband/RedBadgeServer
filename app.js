cd93ec0e61c6723f7de7023ea688c73c347cf9e1
unknown <cbauerband@ivytech.edu>
2020-09-22 18:00:47 -0400

    updated user and admin tables, tested in Postman

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 let express = require('express');
 let app = express();
 let sequelize = require('./db');
+let user = require('./controllers/user-controller');
+let admin = require('./controllers/admin-controller');
 
 sequelize.sync();
 app.use(require('./middleware/headers'));
 app.use(express.json());
+app.use('/api', user);
+app.use('/api', admin)
 app.listen(process.env.PORT, function() {
     console.log('app is listening on port 3000')
 });
